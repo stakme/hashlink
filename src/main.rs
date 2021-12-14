3114c44edca11df61e1f964037644944c8a09128
stakme <stak.me.g@gmail.com>
2021-12-14 19:26:13 +0900

    create symlink (without handling of directories) (#1)
    * generate hashed file name after path components
    
    * refactor
    
    * use env var
    
    * [wip] create symlink

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::os::unix::fs;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "hashlink")]
+struct Opt {
+    #[structopt()]
+    path: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let opt = Opt::from_args();
+
+    let secret: String = env::var("HASHLINK_SECRET_KEY").unwrap_or(String::new());
+    let link = hashlink::encrypt::encrypt(secret, opt.path.clone());
+
+    let mut original = env::current_dir().unwrap();
+    original.push(opt.path);
+
+    let mut dist = env::current_dir().unwrap();
+    dist.push(env::var("HASHLINK_DIST").unwrap_or("dist".to_string()));
+    dist.push(link);
+
+    fs::symlink(original, dist)
 }
